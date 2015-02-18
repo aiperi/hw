b61c961e22d39c0ae8666b611e4c42b3e7ae5c41
aiperi <aiperi.akunova@gmail.com>
2015-02-18 21:55:16 +0600

    Last variant home work

--- hello.rb
+++ hello.rb
@@ -1,27 +1,12 @@
 def greet(variable)
-=begin
-	(string) -> string
-	
-	this method has to accept string
-	and return string
-
-	>  greet("Azamat")
-	=> "Hello, Azamat!"
-=end
-	# write your code here
+  print "Hello,"+variable
 end
+greet("Azamat")
 
 
 def hello
-=begin
-	() -> string
-
-	this method does not have to accept
-	any arguments and it has to return
-	string
-
-	>  hello()
-	=> "Hello!"
-=end
-	# write your code here
+  print"Hello"
 end
+
+hello()
+
